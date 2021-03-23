d145e4f4c6bacf315f15db77d965f939eb869a46
ITKwiatek <ITKwiatek@protonmail.com>
2021-03-23 21:34:17 +0100

    Initial commit

---
+++ src/test/java/com/kwiatek/springsecurityamigos/SpringSecurityAmigosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwiatek.springsecurityamigos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAmigosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
